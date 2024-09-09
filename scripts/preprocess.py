c9cdbb5f1addaa2c7d9f5d749e00e9722d20d2b5
ElbetelTaye <elbeteltaye70@gmail.com>
2024-09-09 16:48:07 +0300

    update the analysis

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -12,6 +12,25 @@ def aggregate_data(df, group_by_column, agg_columns):
     """
     return df.groupby(group_by_column).agg(agg_columns).reset_index()
 
+def get_top_n_values(df, column_name, n=10):
+    """
+    Get the top N largest values in the specified column.
+    """
+    return df.nlargest(n, column_name)
+
+def get_bottom_n_values(df, column_name, n=10):
+    """
+    Get the bottom N smallest values in the specified column.
+    """
+    return df.nsmallest(n, column_name)
+
+def get_most_frequent_value(df, column_name):
+    """
+    Get the most frequent value in the specified column.
+    """
+    return df[column_name].mode()
+
+
 def rename_columns(df, new_column_names):
     """
     Rename columns of the DataFrame.
